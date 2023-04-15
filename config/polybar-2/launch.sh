b8c7657e80210b30217c5bb8bc09251c57bdb689
QuangMinh03042005 <quangminh736.2020@gmail.com>
2023-04-15 14:42:03 +0700

    Thứ bảy, 15 Tháng 4 năm 2023 14:42:03 +07

--- config/polybar-2/launch.sh
+++ config/polybar-2/launch.sh
@@ -5,5 +5,4 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
  
-polybar -c ~/.config/polybar-minimal/config.ini main &
-
+polybar -c ~/.config/polybar-2/config.ini main &
